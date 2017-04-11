3caa2a43b21b3eb658800c8a43ed698881b08a3a
alieu <alieujallow93@gmail.com>
2017-04-11 02:14:43 +0000

    all links working

--- pages/investors.php
+++ pages/investors.php
@@ -20,15 +20,6 @@
 	<!--css links begins-->
     <?php require_once($_SERVER["DOCUMENT_ROOT"].'/MeetYourInvestor/layout/cssLinks.php');?>
     <!--csslinks ends-->
-    
-    <!-- bootstrap theme -->
-    <link href="../css/bootstrap-theme.css" rel="stylesheet">
-    <!-- font icon -->
-    <link href="../css/elegant-icons-style.css" rel="stylesheet" />
-    <link href="../css/font-awesome.min.css" rel="stylesheet" />    
-    <!-- Custom styles -->
-    <link href="../css/style.css" rel="stylesheet">
-    <link href="../css/style-responsive.css" rel="stylesheet" />
 	
 </head>
 <body>
--- pages/startupEditProfile.php
+++ pages/startupEditProfile.php
@@ -15,8 +15,6 @@
 	<!--css links begins-->
     <?php require_once($_SERVER["DOCUMENT_ROOT"].'/MeetYourInvestor/layout/cssLinks.php');?>
     <!--csslinks ends-->
-
-	
 </head>
 <body>
 
@@ -131,14 +129,15 @@
 	<!--footer ends-->
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="js/jquery-2.1.1.min.js"></script>		
+	<script src="../js/jquery-2.1.1.min.js"></script>		
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="js/bootstrap.min.js"></script>	
-	<script src="js/parallax.min.js"></script>
-	<script src="js/wow.min.js"></script>
-	<script src="js/jquery.easing.min.js"></script>
-	<script type="text/javascript" src="js/fliplightbox.min.js"></script>
-	<script src="js/functions.js"></script>
-	<script src="contactform/contactform.js"></script>
+	<script src="../js/bootstrap.min.js"></script>	
+	<script src="../js/parallax.min.js"></script>
+	<script src="../js/wow.min.js"></script>
+	<script src="../js/jquery.easing.min.js"></script>
+	<script type="text/javascript" src="../js/fliplightbox.min.js"></script>
+	<script src="../js/functions.js"></script>
+	<script src="../contactform/contactform.js"></script>
+	<script src="../js/ourjs.js"></script>
 </body>
 </html>
